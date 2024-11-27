7370aa6199ae817b0a7f7dbb84003262f002ae46
Adam-Kosciukiewicz <adamkosciukiewiczkontakt@gmail.com>
2024-11-27 08:46:46 +0100

    feat: add empty list label component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+import EmptyListLabel from "@/components/EmptyListLabel";
+import React, { useState } from "react";
+
+interface navLink {
+  name: string;
+  link: string;
+}
+
+
 export default function Home() {
+  const [navLinks, setNavLinks] = useState<navLink[]>([])
   return (
-    <div className="font-[family-name:var(--font-inter)]">
-     
-    </div>
+    <div className="font-[family-name:var(--font-inter)] w-[100%] h-[100%] bg-[#f5f4f4] py-[30px] px-[24px]">
+      {navLinks.length ? (
+        <></>
+      ) : (
+        <EmptyListLabel
+          title="Menu jest puste"
+          subtitle="W tym menu nie ma jeszcze żadnych linków"
+          buttonText="Dodaj pozycje menu"
+        />
+      )
+      }
+    </div >
   );
 }
